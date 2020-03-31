507b0db411aeb05ce9da223c8ea8f4ad6b46f345
ulvi1 <ulvi.1999rufet@gmail.com>
2020-03-31 13:51:57 +0400

    Login Design

---
+++ lib/CustomIcons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon {
+
+  static const IconData twitter =  IconData(0xe900, fontFamily: 'CustomIcons');
+  static const IconData facebook = IconData(0xe901, fontFamily: 'CustomIcons');
+  static const IconData googlePlus = IconData(0xe902, fontFamily: 'CustomIcons');
+  static const IconData linkedin = IconData(0xe903, fontFamily: 'CustomIcons');
+
+
+}
